750d793a035801322ed946d5cc742815138ed795
JAYAPRAKASH <ajayaprakash98@gmail.com>
2020-09-09 03:13:32 -0700

    Create The Creator_Square.py

---
+++ The Creator_Square.py
@@ -0,0 +1,108 @@
+'''        QUESTION:
+
+The Creator - Square:
+
+Lying of the roof top, at night time, enjoying thee cold breeze, seeing the stars is a enjoyable moment. It is believed that if we think of a particular shape in in mind, 
+and look at the stars, it will seem like the stars are aligned in that particular pattern.
+
+Emma is a 5 year old kid, she is looking at the stars and expecting the stars to come in a particular pattern. 
+Victor has a super power to make the stars come in a particular alignment. Can you help Victor to make Emma happy.
+
+Emma thinks of a square shape now.
+
+Input Format : A single integer input N, where N is the size of the grid
+
+Input Constraints : 1<=n<=50
+
+Output Format : Make the pattern visible for Emma
+
+Sample Input :
+5
+
+Sample Output :
+*****
+*****
+*****
+*****
+*****
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+HINTS:
+
+1 - How do you form a square?
+
+2 - Input = 5
+    This should have 5 lines of output.
+    Each line should have 5 stars printed out to form the pattern that is required.
+
+3 - Input = 6
+    This should have 6 lines of output.
+    Each line should have 6 stars printed out to form the pattern that is required.
+    
+    '''
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# ANSWER:
+
+x = int(input())
+
+for i in range(x):
+  print("*"*x)
+    
+    
+
